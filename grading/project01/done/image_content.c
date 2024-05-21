730bb3303e38ed0bc22e6403bbf39c298e788f2d
Jean-Cédric Chappelier <jean-cedric.chappelier@epfl.ch>
2024-05-21 15:31:00 +0200

    feedback

---
+++ grading/project01/done/image_content.c
@@ -0,0 +1,143 @@
+#include "image_content.h"
+#include "imgfs.h"
+#include <vips/vips.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Index of the width value for different resolutions
+#define THUMB_RES_WIDTH_INDEX 0
+#define SMALL_RES_WIDTH_INDEX 2
+
+// Error if the ftell function fails
+#define ERR_FTELL -1
+
+#define OFFSET_ZERO 0
+
+/**
+ * @brief Free memory allocated to the original and resized images, as well as their corresponding VipsImages
+ *
+ * @param original_image Pointer to the original image
+ * @param resized_image Pointer to the resized image
+ * @param original_vipsimage Pointer to the original VipsImage
+ * @param resized_vipsimage Pointer to the resized VipsImage
+ *
+ * @return some error code
+ */
+int free_images(void *original_image, void *resized_image, VipsImage *original_vipsimage, VipsImage *resized_vipsimage)
+{
+    free(original_image);
+    free(resized_image);
+    g_object_unref(original_vipsimage);
+    g_object_unref(resized_vipsimage);
+    return ERR_NONE;
+}
+
+/**********************************************************************
+ * Resize the image to the given resolution, if needed.
+ ********************************************************************** */
+int lazily_resize(int resolution, struct imgfs_file *imgfs_file, size_t index)
+{ // Check if arguments are valid
+    if (resolution != THUMB_RES && resolution != SMALL_RES && resolution != ORIG_RES)
+        return ERR_INVALID_ARGUMENT; // grader: if resolution is not valid, you should return ERR_RESOLUTIONS,
+        // except for ORIG_RES which should be ERR_NONE -0.5
+
+    M_REQUIRE_NON_NULL(imgfs_file);
+    M_REQUIRE_NON_NULL(imgfs_file->file);
+
+    if (index < 0 || index >= imgfs_file->header.max_files)
+        return ERR_INVALID_IMGID;
+
+    if (!imgfs_file->metadata[index].is_valid) // grader: check relies on the fact that IS_VALID is 1, use the enum value, -0.5
+        return ERR_INVALID_IMGID;
+
+    if (resolution == ORIG_RES)
+        return ERR_NONE;
+
+    // Check if image already exists in given resolution
+    if (imgfs_file->metadata[index].size[resolution])
+        return ERR_NONE;
+
+    // Find the correct width according to the resolution
+    uint16_t width = (resolution == THUMB_RES) ? imgfs_file->header.resized_res[THUMB_RES_WIDTH_INDEX] : imgfs_file->header.resized_res[SMALL_RES_WIDTH_INDEX];
+
+    if (fseek(imgfs_file->file, (long)imgfs_file->metadata[index].offset[ORIG_RES], SEEK_SET))
+    {
+        return ERR_IO;
+    }
+
+    // Resize the original image to the requested resolution and free allocated memory in case of error
+    void *orig_img = calloc(1, imgfs_file->metadata[index].size[ORIG_RES]);
+    // grader: you should check if orig_img is NULL, -1
+    if (fread(orig_img, imgfs_file->metadata[index].size[ORIG_RES], ONE_ELEMENT, imgfs_file->file) != ONE_ELEMENT)
+    {
+        free(orig_img);
+        return ERR_IO;
+    }
+
+    VipsImage *vips_orig_img = NULL;
+    if (vips_jpegload_buffer(orig_img, imgfs_file->metadata[index].size[ORIG_RES], &vips_orig_img, NULL))
+    {
+        free(orig_img);
+        return ERR_IO;
+    }
+
+    VipsImage *vips_resized_img = NULL;
+    if (vips_thumbnail_image(vips_orig_img, &vips_resized_img, width, NULL))
+        // grader: missing other arguments, works because our images are square, but wouldn't work if we want to resize
+        // to non-square images, -1
+    {
+        free(orig_img);
+        g_object_unref(vips_orig_img);
+        return ERR_IO;
+    }
+
+    size_t len = 0;
+    void *resized_img = NULL;
+    if (vips_jpegsave_buffer(vips_resized_img, &resized_img, &len, NULL))
+    {
+        free(orig_img);
+        g_object_unref(vips_orig_img);
+        g_object_unref(vips_resized_img);
+        return ERR_IO;
+    }
+
+    // Write the resized image at the end of the file
+    if (fseek(imgfs_file->file, OFFSET_ZERO, SEEK_END))
+    {
+        free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+        return ERR_IO;
+    }
+
+    if (fwrite(resized_img, len, ONE_ELEMENT, imgfs_file->file) != ONE_ELEMENT)
+    {
+        free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+        return ERR_IO;
+    }
+
+    // Update the metadata with the correct size and offset, then write it back to the file
+    imgfs_file->metadata[index].size[resolution] = (uint32_t)len;
+    long end_of_file = ftell(imgfs_file->file);
+
+    if (end_of_file == ERR_FTELL)
+    {
+        free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+        return ERR_IO;
+    }
+
+    imgfs_file->metadata[index].offset[resolution] = (uint64_t)end_of_file - len;
+
+    if (fseek(imgfs_file->file, sizeof(imgfs_file->header), SEEK_SET))
+    {
+        free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+        return ERR_IO;
+    }
+
+    if (fwrite(imgfs_file->metadata, sizeof(struct img_metadata), imgfs_file->header.max_files, imgfs_file->file) != imgfs_file->header.max_files)
+    {
+        // grader: you should write only one element of the metadata array, not the whole array, -1
+        free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+        return ERR_IO;
+    }
+
+    return free_images(orig_img, resized_img, vips_orig_img, vips_resized_img);
+}
---
+++ grading/project01/done/imgfscmd_functions.c
@@ -0,0 +1,208 @@
+/**
+ * @file imgfscmd_functions.c
+ * @brief imgFS command line interpreter for imgFS core commands.
+ *
+ * @author Mia Primorac
+ */
+
+#include "imgfs.h"
+#include "imgfscmd_functions.h"
+#include "util.h" // for _unused
+
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+// default values
+static const uint32_t default_max_files = 128;
+static const uint16_t default_thumb_res = 64;
+static const uint16_t default_small_res = 256;
+
+// max values
+static const uint16_t MAX_THUMB_RES = 128;
+static const uint16_t MAX_SMALL_RES = 512;
+
+#define FILE_NAME_INDEX 0
+#define IMG_ID_INDEX 1
+
+// The number of arguments for max_files, thumb_res and small_res
+#define MAX_FILES_ARG 1
+#define WIDTH_AND_HEIGHT_ARGS 2
+
+#define TWO_ELEMENTS 2
+
+/**********************************************************************
+ * Displays some explanations.
+ ********************************************************************** */
+int help(int useless _unused, char **useless_too _unused)
+{
+    /* **********************************************************************
+     * TODO WEEK 08: WRITE YOUR CODE HERE.
+     * **********************************************************************
+     */
+
+    printf("imgfscmd [COMMAND] [ARGUMENTS]\n");
+    printf("  help: displays this help.\n");
+    printf("  list <imgFS_filename>: list imgFS content.\n");
+    printf("  create <imgFS_filename> [options]: create a new imgFS.\n");
+    printf("      options are:\n");
+    printf("          -max_files <MAX_FILES>: maximum number of files.\n");
+    printf("                                  default value is 128\n"); //grader: hardcoded defaults, -0.5
+    printf("                                  maximum value is 4294967295\n");
+    printf("          -thumb_res <X_RES> <Y_RES>: resolution for thumbnail images.\n");
+    printf("                                  default value is 64x64\n");
+    printf("                                  maximum value is 128x128\n");
+    printf("          -small_res <X_RES> <Y_RES>: resolution for small images.\n");
+    printf("                                  default value is 256x256\n");
+    printf("                                  maximum value is 512x512\n");
+    printf("  delete <imgFS_filename> <imgID>: delete image imgID from imgFS.\n");
+
+    return ERR_NONE;
+}
+
+/**********************************************************************
+ * Opens imgFS file and calls do_list().
+ ********************************************************************** */
+int do_list_cmd(int argc, char **argv)
+{
+    /* **********************************************************************
+     * TODO WEEK 07: WRITE YOUR CODE HERE.
+     * **********************************************************************
+     */
+
+    // Check if arguments are valid
+    M_REQUIRE_NON_NULL(argv);
+
+    if (argc != ONE_ELEMENT)
+        return ERR_INVALID_COMMAND;
+
+    const char *file_name = argv[FILE_NAME_INDEX];
+    M_REQUIRE_NON_NULL(file_name);
+
+    int ret = ERR_NONE;
+
+    // Create a new imgfs_file and initialize its content to zero
+    struct imgfs_file file_to_create;
+    memset(&file_to_create, 0, sizeof(file_to_create));
+
+    ret = do_open(file_name, "rb", &file_to_create);
+    if (ret != ERR_NONE)
+        return ret;
+
+    ret = do_list(&file_to_create, STDOUT, NULL);
+    if (ret != ERR_NONE)
+        return ret;
+
+    do_close(&file_to_create);
+
+    return ret;
+}
+
+/**********************************************************************
+ * Prepares and calls do_create command.
+ ********************************************************************** */
+int do_create_cmd(int argc, char **argv)
+{
+    // puts("Create");
+    /* **********************************************************************
+     * TODO WEEK 08: WRITE YOUR CODE HERE (and change the return if needed).
+     * **********************************************************************
+     */
+
+    // Check if arguments are valid
+    M_REQUIRE_NON_NULL(argv);
+    if (argc == EMPTY)
+        return ERR_NOT_ENOUGH_ARGUMENTS;
+
+    const char *imgfs_filename = argv[FILE_NAME_INDEX];
+    uint32_t max_files = default_max_files;
+    uint16_t thumb_width = default_thumb_res, thumb_height = default_thumb_res,
+             small_width = default_small_res, small_height = default_small_res;
+
+    if (argc > ONE_ELEMENT)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            if (!strcmp(argv[i], "-max_files"))
+            {
+                // Make sure that the "max_files" value is present
+                if (i + MAX_FILES_ARG >= argc)
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                max_files = atouint32(argv[i + 1]);
+                i += ONE_ELEMENT; // Skip the next argument, since it's the max_files value
+            }
+            else if (!strcmp(argv[i], "-thumb_res"))
+            {
+                // Make sure that the "thumb_res" values are present
+                if (i + WIDTH_AND_HEIGHT_ARGS >= argc)
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                thumb_width = atouint16(argv[i + 1]);  // Get width value
+                thumb_height = atouint16(argv[i + 2]); // Get height value
+                i += TWO_ELEMENTS;
+            }
+            else if (!strcmp(argv[i], "-small_res"))
+            {
+                // Make sure that the "small_res" values are present
+                if (i + WIDTH_AND_HEIGHT_ARGS >= argc)
+                    return ERR_NOT_ENOUGH_ARGUMENTS;
+                small_width = atouint16(argv[i + 1]);
+                small_height = atouint16(argv[i + 2]);
+                i += TWO_ELEMENTS;
+            }
+            else
+                return ERR_INVALID_ARGUMENT;
+        }
+    }
+    if (max_files == 0)
+        return ERR_INVALID_ARGUMENT;
+    if (max_files > default_max_files)
+        return ERR_MAX_FILES;
+    if (thumb_width <= 0 || thumb_width > MAX_THUMB_RES || thumb_height <= 0 || thumb_height > MAX_THUMB_RES)
+        return ERR_RESOLUTIONS;
+    if (small_width <= 0 || small_width > MAX_SMALL_RES || small_height <= 0 || small_height > MAX_SMALL_RES)
+        return ERR_RESOLUTIONS;
+
+    struct imgfs_header header = {.max_files = max_files,
+                                  .resized_res = {thumb_width, thumb_height, small_width, small_height}};
+
+    struct imgfs_file imgfs_file;
+    imgfs_file.header = header;
+
+    int return_value = do_create(imgfs_filename, &imgfs_file);
+    if (return_value == ERR_NONE)
+        do_close(&imgfs_file);
+    return return_value;
+}
+
+/**********************************************************************
+ * Deletes an image from the imgFS.
+ */
+int do_delete_cmd(int argc, char **argv)
+{
+    /* **********************************************************************
+     * TODO WEEK 08: WRITE YOUR CODE HERE (and change the return if needed).
+     * **********************************************************************
+     */
+
+    // Check if arguments are valid
+    M_REQUIRE_NON_NULL(argv);
+    if (argc != TWO_ELEMENTS)
+        return ERR_NOT_ENOUGH_ARGUMENTS;
+
+    const char *filename = argv[FILE_NAME_INDEX];
+    const char *img_ID = argv[IMG_ID_INDEX];
+    if ((img_ID == NULL) || (strlen(img_ID) > MAX_IMG_ID))
+        return ERR_INVALID_IMGID;
+
+    struct imgfs_file imgfs_file;
+    int ret = ERR_NONE;
+    
+    ret = do_open(filename, "r+b", &imgfs_file);
+    if (ret != ERR_NONE)
+        return ret;
+
+    ret = do_delete(img_ID, &imgfs_file);
+    do_close(&imgfs_file);
+
+    return ret;
+}
